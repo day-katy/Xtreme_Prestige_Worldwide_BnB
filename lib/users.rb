862c2739008fdf28573ccd2fd35a3043798d681b
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-02-01 15:54:20 +0000

    created create for user

---
+++ lib/users.rb
@@ -0,0 +1,29 @@
+require 'pg'
+
+class Users
+  attr_reader :id, :email, :password, :name
+
+  def initialize(id:, email:, password:, name:)
+    @id = id
+    @email = email
+    @password = password
+    @name = name
+  end
+
+  def self.create(email:, password:, name:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'makers_bnb_test')
+    else
+      connection = PG.connect(dbname: 'makers_bnb')
+    end
+
+    result = connection.exec("INSERT INTO users (email, password, name)
+                              VALUES ('#{email}', '#{password}', '#{name}')
+                              RETURNING id, email, password, name ;")
+
+    Users.new(id: result[0]['id'],email: result[0]['email'],password: result[0]['password'],name: result[0]['name'])
+
+
+  end
+
+end
---
+++ spec/users_spec.rb
@@ -0,0 +1,18 @@
+require 'users.rb'
+
+describe Users do
+  it 'allows you to make an account and sign in' do
+    test_user = Users.new(id: 1, email: 'test@email.com', password: 'password', name: 'Jack')
+    expect(test_user.id).to eq 1
+    expect(test_user.email).to eq 'test@email.com'
+    expect(test_user.password).to eq 'password'
+    expect(test_user.name).to eq 'Jack'
+  end
+
+  it 'can recall the user from the database' do
+    Users.create(email: 'test@email.com', password: 'password', name: 'Charlie')
+    expect(Users.all_users[0].name).to eq 'Charlie'
+    expect(Users.all_users[0].email).to eq 'test@email.com'
+    expect(Users.all_users[0].password).to eq 'password'
+  end 
+end
