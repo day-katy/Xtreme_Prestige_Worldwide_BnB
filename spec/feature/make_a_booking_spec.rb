6107635360a3a14ed1fc5436973c2de7945b79d7
Katy Day <day.katy@gmail.com>
2021-02-04 15:15:17 +0000

    trying to fix things

--- spec/feature/make_a_booking_spec.rb
+++ spec/feature/make_a_booking_spec.rb
@@ -1,8 +1,9 @@
 feature 'Booking a listing' do
   scenario 'it allows you to book the property' do
-    DatabaseConnection.query("INSERT INTO listings (name, free_date) VALUES ('Oscar''s Parisian Flat', 'June 1, 2021');")
+
+    DatabaseConnection.query("INSERT INTO listings (name, free_date) VALUES ('Oscars Parisian Flat', 'June 1, 2021');")
     visit('/listings')
     click_button('Book')
-    expect(page).to have_content "Your booking request has been sent!"
+    expect(page).to have_content 'Your booking was confirmed!' 
   end
 end
