39de91925428cab29be513a21635c1931c824924
Jack Shields <71281209+jshields123@users.noreply.github.com>
2021-02-04 14:45:56 +0000

    Update 01_create_table_listings.sql

--- db/migrations/01_create_table_listings.sql
+++ db/migrations/01_create_table_listings.sql
@@ -1,1 +1,1 @@
-CREATE TABLE listings (id SERIAL PRIMARY KEY, name VARCHAR(60), free_date DATE, host_id INTEGER REFERENCES users (id));
+CREATE TABLE listings (id SERIAL PRIMARY KEY, name VARCHAR(60), free_date DATE, host_id INTEGER REFERENCES users (user_id));
