11417f3b020ca823b84d5ebbd79d6e0c6e03a9b1
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-04-06 22:04:08 +0100

    amended db migrations

--- db/migrations/01_create_table_listings.sql
+++ db/migrations/01_create_table_listings.sql
@@ -1,1 +1,1 @@
-CREATE TABLE listings (listing_id SERIAL PRIMARY KEY, name VARCHAR(60), free_date DATE, host_id INTEGER REFERENCES users (user_id));
+CREATE TABLE listings (listing_id SERIAL PRIMARY KEY, name VARCHAR(60), free_date DATE, );
--- db/migrations/04_update_table_columns.sql
+++ db/migrations/04_update_table_columns.sql
@@ -1,3 +1,9 @@
+ALTER TABLE listings ADD COLUMN host_id INTEGER REFERENCES users (user_id);
+
+ALTER TABLE "public"."listings" ADD COLUMN "image" varchar;
+ALTER TABLE "public"."listings" ADD COLUMN "description" varchar(120);
+ALTER TABLE "public"."listings" ADD COLUMN "price" varchar(60);
+
 # Steps
 
 * Changed id in listings to listing_id
